f48e695fccfa3bdea395cea451ba88aa8cb691df
csecluki <lukaszzagorski99@gmail.com>
2023-02-25 22:48:03 +0100

    Update settings.py

--- restarter.sh
+++ restarter.sh
@@ -1,7 +1,20 @@
 #!/bin/bash
 
-ls
+echo Stopping all running containers
 sudo docker stop $(sudo docker ps -a -q)
+echo Containers stopped
+echo
+
+echo Removing all containers
 sudo docker rm -f $(sudo docker ps -a -q)
+echo Containers removed
+echo
+
+echo Removing all images
 sudo docker rmi $(sudo docker images -q)
+echo Images removed
+echo
+
+echo Starting...
 sudo docker compose up --build
+
