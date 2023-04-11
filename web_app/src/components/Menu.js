e821516cb3f4b1199aca1dfefd05d52d883b29f3
csecluki <lukaszzagorski99@gmail.com>
2023-04-11 21:04:32 +0200

    Add menu button to menu

--- web_app/src/components/Menu.js
+++ web_app/src/components/Menu.js
@@ -4,6 +4,7 @@ const Menu = () => {
 
     return (
         <div>
+            <Link to={''} className="menu-item">Menu</Link>
             <Link to={'/accounts'} className="menu-item">Accounts</Link>
             <Link to={'/sessions'} className="menu-item">Sessions</Link>
         </div>
